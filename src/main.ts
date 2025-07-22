05a3fa2d77293a6241b8d974bdecee5a979b7e72
chenxi <xiaozhangnike2@gmail.com>
2025-07-22 18:19:20 +0800

    #feat: cors

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,14 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // ✅ 开启跨域
+  app.enableCors({
+    origin: '*', // 生产环境可改为 'https://thinkingking.top'
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
