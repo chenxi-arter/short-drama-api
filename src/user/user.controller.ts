e2b96c9096f5b8870aa05d3e436b5421a0662a8f
chenxi <xiaozhangnike2@gmail.com>
2025-07-22 15:30:47 +0800

    #feat:add  telegram-login get

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, Get, Query } from '@nestjs/common';
 import { UserService } from './user.service';
 import { TelegramUserDto } from './dto/telegram-user.dto';
 
@@ -8,6 +8,12 @@ export class UserController {
 
   @Post('telegram-login')
   async telegramLogin(@Body() dto: TelegramUserDto) {
+    return this.userService.telegramLogin(dto);
+  }
+  @Get('telegram-login')
+  async telegramLoginGet(
+    @Query() dto: TelegramUserDto, // 用 @Query 接收 query 参数
+  ) {
     return this.userService.telegramLogin(dto);
   }
 }
