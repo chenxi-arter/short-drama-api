e9c4d94d180b3db93e44f64870c2b3980475a232
ChenXi <chenxi@example.com>
2025-07-26 15:55:13 +0800

    #feat:Configure multi-core support by integrating the cluster module.

---
+++ ecosystem.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  apps: [
+    {
+      name: 'short-drama-api',   // pm2 ls 里看到的名字
+      script: 'dist/main.js',    // build 后的入口
+      cwd: '/home/deploy/workspace/short-drama-api',
+      instances: 'max',          // 核数满额多进程，可改成 1
+      exec_mode: 'cluster',      // 负载均衡模式（单实例可改 fork）
+      env: {
+        NODE_ENV: 'production',
+        PORT: 3001              // 你的 Nest 监听端口
+      },
+      log_date_format: 'YYYY-MM-DD HH:mm:ss Z',
+      log_file: './logs/combined.log',
+      out_file: './logs/out.log',
+      error_file: './logs/error.log',
+      merge_logs: true,
+      max_memory_restart: '1G'   // 内存重启阈值
+    }
+  ]
+};
